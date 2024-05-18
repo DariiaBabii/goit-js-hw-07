678194cff55d4cadff02fafacb29f1e774ecb860
dariia_babii <dariia.babii02@gmail.com>
2024-05-18 19:38:10 +0200

    events

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#controls input[type="number"]');
+const boxes = document.querySelector("#boxes");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value, 10);
+  if (isNaN(amount) || amount <= 0 || amount > 100) {
+    return alert("Please enter a valid number (between 1 - 100)");
+  }
+
+  createBoxes(amount);
+  input.value = "";
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  destroyBoxes();
+  const elements = [];
+  for (let i = 0; i < amount; i++) {
+    const element = document.createElement("div");
+    element.style.width = `${30 + i * 10}px`;
+    element.style.height = `${30 + i * 10}px`;
+    element.style.backgroundColor = getRandomHexColor();
+    elements.push(element);
+  }
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
